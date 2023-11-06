be97d6d425ecb9d6ef72cc8b4880616fc12e7f66
ramki-chatgen <ramakrishna@chatgen.ai>
2023-11-06 02:34:39 +0000

    setup eslint

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,11 @@
 import { render } from "solid-js/web";
 import App from "./App";
 
-render(() => <App />, document.getElementById("root")!);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  render(() => <App />, rootElement);
+} else {
+  //   console.error("Could not find root element");
+  throw new Error("Could not find root element");
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "vite";
 import suidPlugin from "@suid/vite-plugin";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [suidPlugin(), solidPlugin()],
+  server: {
+    port: 3000,
+  },
   build: {
     target: "esnext",
   },
