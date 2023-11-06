3473e1ae1264a3752c4219f2e75fcbea5a5167d5
ramki-chatgen <ramakrishna@chatgen.ai>
2023-11-06 09:29:43 +0000

    updated sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,25 +1,47 @@
 import { NavLink } from "@solidjs/router";
 import { List, ListItem, ListItemText } from "@suid/material";
+import Drawer from "@suid/material/Drawer";
+import { styled, useTheme } from '@suid/material/styles';
+import { yellow } from "@suid/material/colors";
 
 const SideBar = () => {
+  const theme = useTheme();
+
+  // Create a styled Drawer with theme overrides
+  const StyledDrawer = styled(Drawer)({
+    width: 240,
+    flexShrink: 0,
+    '& .MuiDrawer-paper': {
+      width: 240,
+      boxSizing: 'border-box',
+      backgroundColor: theme.palette.primary.dark, // use the dark shade of the primary color
+      color: yellow[500],
+    },
+  });
+
+  const StyledNavLink = styled(NavLink)({
+    color: 'inherit', // The color will be inherited from the parent styling
+    textDecoration: 'none', // Removing the underline or other text decorations
+    // '&:hover, &:focus': {
+    //   color: 'inherit', // Ensure the color is inherited even on hover/focus
+    // },
+  });
+
+  const StyledListItemText = styled(ListItemText)({
+    color: 'inherit', // The color will be inherited from the parent styling
+  });
+
   return (
-    <List>
-      <ListItem component={NavLink} href="/dashboard" end>
-        <ListItemText primary="Dashboard" />
-      </ListItem>
-      <ListItem component={NavLink} href="/getting-started">
-          <ListItemText primary="Getting Started" />
-        </ListItem>
+    <StyledDrawer variant="permanent" anchor="left">
       <List>
-        <ListItem component={NavLink} href="/getting-started/overview">
-          <ListItemText primary="Overview" />
+        <ListItem component={StyledNavLink} href="/">
+          <StyledListItemText primary="Dashboard" />
         </ListItem>
-        <ListItem component={NavLink} href="/getting-started/usage">
-          <ListItemText primary="Usage" />
+        <ListItem component={StyledNavLink} href="/getting-started">
+          <StyledListItemText primary="Getting started" />
         </ListItem>
       </List>
-      {/* Add other sidebar items here */}
-    </List>
+    </StyledDrawer>
   );
 };
 
